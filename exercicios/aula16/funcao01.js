c8d25cb0b430f4f8b1a727b7d203ba29bf38d721
Haru <75090262+matheus-haru@users.noreply.github.com>
2023-12-20 09:38:37 -0300

    Aula16

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimp(10)
+console.log(res)
+/* OU
+console.log(parimp(10))
+*/
---
+++ exercicios/aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // "=0" se não receber valor, fica 0
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
